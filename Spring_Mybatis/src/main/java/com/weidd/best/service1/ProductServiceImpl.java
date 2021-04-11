7215fe421a485a3a9614c31c6cf461a375a2234b
weidong902 <876074235@qq.com>
2021-04-11 21:23:32 +0800

    spring-mybatis 整合 事务控制
    整合Spring配置文件配置:
        <!--注入数据源对象 Druid-->
        <!--注入SqlSessionFactory-->
        <!--注入dao-->
        <!--创建事务管理器 TransactionManager-->
        <!--根据事务管理器生成一个事务的环绕通知-->
        <!--组装切面-->
        <!--业务层-->

--- Spring_Mybatis/src/main/java/com/weidd/best/service1/ProductServiceImpl.java
+++ Spring_Mybatis/src/main/java/com/weidd/best/service1/ProductServiceImpl.java
@@ -2,6 +2,8 @@ package com.weidd.best.service1;
 
 import com.weidd.best.dao1.ProductDao;
 import com.weidd.best.pojo.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -13,13 +15,15 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
 
     private ProductDao productDao;
+
     public void setProductDao(ProductDao productDao) {
         this.productDao = productDao;
     }
 
     @Override
     public void save(Product product) {
-
+        productDao.saveProduct(product);
+        throw new RuntimeException("保存失败...");
     }
 
     @Override
--- Spring_Mybatis/src/main/java/com/weidd/best/test/TestMybatis.java
+++ Spring_Mybatis/src/main/java/com/weidd/best/test/TestMybatis.java
@@ -2,10 +2,12 @@ package com.weidd.best.test;
 
 import com.weidd.best.pojo.Product;
 import com.weidd.best.service1.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * @program: SpringAll
@@ -13,10 +15,13 @@ import java.util.List;
  * @date: 2021-04-10 21:23
  **/
 public class TestMybatis {
+
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("mybatis2.xml");
         ProductService productService = (ProductService) context.getBean("productService");
+        productService.save(new Product("奥迪 "+ System.currentTimeMillis(),500000));
+
         List<Product> list = productService.find();
         for (Product product : list) {
             System.out.println(product);
